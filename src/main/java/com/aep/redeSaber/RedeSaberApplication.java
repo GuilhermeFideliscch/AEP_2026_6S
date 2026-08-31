cae6b4bfd177c99133ce620285fe9206e039a374
GuilhermeFideliscch <guilherme.fidelis2570@gmail.com>
2026-08-31 16:33:58 -0300

    cria base do backend, testa api com HealthController

---
+++ src/main/java/com/aep/redeSaber/RedeSaberApplication.java
@@ -0,0 +1,13 @@
+package com.aep.redeSaber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeSaberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeSaberApplication.class, args);
+	}
+
+}
